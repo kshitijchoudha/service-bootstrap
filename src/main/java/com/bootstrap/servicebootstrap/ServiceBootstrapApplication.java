e2d6d7b7aec667b70e8058003b63f04e7d03d253
KC <kchoudha@gmail.com>
2022-12-21 17:38:02 -0600

    change

--- src/main/java/com/bootstrap/servicebootstrap/ServiceBootstrapApplication.java
+++ src/main/java/com/bootstrap/servicebootstrap/ServiceBootstrapApplication.java
@@ -10,6 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServiceBootstrapApplication implements CommandLineRunner {
 
 	Logger logger = LoggerFactory.getLogger(getClass());
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceBootstrapApplication.class, args);
@@ -22,46 +31,50 @@ public class ServiceBootstrapApplication implements CommandLineRunner {
 		String serviceName = "";
 		String templateName = "";
 
-		System.out.println("Enter Service Name: ");
+		System.out.print("Enter Service Name: ");
 		serviceName = System.console().readLine();
 
-		System.out.println(String.join("\n", "Enter number for service type and deployment type template to use",
-				"1. Spring Boot with Helm Deployment", "2. Python Flask with kuberetes manifest deployment"));
+		System.out.println(
+				String.join("\n", ANSI_BLUE + "Enter number for service type and deployment type template to use",
+						"1. Spring Boot with Helm Deployment",
+						"2. Python Flask with kuberetes manifest deployment" + ANSI_BLUE));
 
 		templateName = System.console().readLine();
 
-		if(templateName == null || (!templateName.trim().equalsIgnoreCase("1") && !templateName.trim().equalsIgnoreCase("2")))
-		{
-			System.out.println("Only options are 1 or 2, you entered: "+templateName+"END");
+		if (templateName == null
+				|| (!templateName.trim().equalsIgnoreCase("1") && !templateName.trim().equalsIgnoreCase("2"))) {
+			System.out.println(ANSI_RED + "Only options are 1 or 2, you entered: " + ANSI_RED + templateName);
 			return;
 		}
 
-		templateName = templateName.trim().equalsIgnoreCase("1") ? "spring-boot-helm-template" : "flask-k8s-mf-template";
+		templateName = templateName.trim().equalsIgnoreCase("1") ? "spring-boot-helm-template"
+				: "flask-k8s-mf-template";
 
 		String envName = serviceName + "-env-1";
 		String infraDefName = envName + "-infra-def-1";
 		String pipelineName = serviceName + "-pipeline-1";
-		
+
 		HarnessClient harnessClient = new HarnessClient();
 
-		System.out.println("Bootstraping service: "+serviceName +" ,with template: "+templateName + "\n" + "Continue?[Y/n]:");
+		System.out.print(
+				ANSI_BLUE + "Bootstraping service: " + serviceName + " ,with template: " + templateName + "\n"
+						+ "Continue?[Y/n]:" + ANSI_BLUE);
 		String proceed = System.console().readLine();
 
-		if(proceed == null || (proceed!=null && proceed.trim().equalsIgnoreCase("Y")) || (proceed!=null && proceed.trim().equalsIgnoreCase("")))
-		{
-			System.out.println("Creating Github, Harness and Terrform resources..");
+		if (proceed == null || (proceed != null && proceed.trim().equalsIgnoreCase("Y"))
+				|| (proceed != null && proceed.trim().equalsIgnoreCase(""))) {
+			System.out.println(ANSI_GREEN + "Creating Github, Harness and Terrform resources.." + ANSI_GREEN);
+
+		} else {
+			System.out.println(ANSI_RED + "Aborting...." + ANSI_RED);
+			return;
 		}
-        else
-        {
-            System.out.println("Aborting....");
-            return;
-        }
-		
 
 		try {
 			harnessClient.createHarnessEnvironment(envName);
 			logger.info("Created Harness Environment: " + envName);
-			String deploymentType = templateName.trim().equalsIgnoreCase("spring-boot-helm-template") ? "NativeHelm" : "Kubernetes";
+			String deploymentType = templateName.trim().equalsIgnoreCase("spring-boot-helm-template") ? "NativeHelm"
+					: "Kubernetes";
 			harnessClient.createHarnessInfraDefinition(infraDefName,
 					envName.replace("-", ""), deploymentType, "default");
 			// harnessClient.createHarnessK8SConnector("auto-k8s-connector-kind","k8s-kind-mac-delegate");
